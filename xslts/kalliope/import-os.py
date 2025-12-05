d89d5c06a25a51e6500a02e7383aa401051adaf7
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2025-12-05 09:43:55 +0100

    Kleinigkeit

--- xslts/kalliope/import-os.py
+++ xslts/kalliope/import-os.py
@@ -2,10 +2,14 @@ import requests
 import xml.etree.ElementTree as ET
 
 url = "https://kalliope-verbund.info/sru?version=1.2&operation=searchRetrieve&query=Arthur+Schnitzler&recordSchema=mods"
+headers = {
+    "Content-type": "application/xml; charset=utf-8",
+    "Accept-Charset": "utf-8",
+}
 
 # Get the number of records from the SRU endpoint
-response = requests.get(url + "&maximumRecords=0")
-root = ET.fromstring(response.content)
+response = requests.get(url + "&maximumRecords=0", headers=headers)
+root = ET.fromstring(response.content.decode("utf-8"))
 num_records = int(root.find(".//{http://www.loc.gov/zing/srw/}numberOfRecords").text)
 
 # Set the batch size and initialize counters
@@ -22,8 +26,8 @@ with open("results.xml", "wb") as f:
     while start_record <= num_records:
 
         # Get the records for the current batch
-        response = requests.get(url + f"&startRecord={start_record}&maximumRecords={batch_size}")
-        root = ET.fromstring(response.content)
+        response = requests.get(url + f"&startRecord={start_record}&maximumRecords={batch_size}", headers=headers)
+        root = ET.fromstring(response.content.decode("utf-8"))
         records = root.findall(".//{http://www.loc.gov/mods/v3}mods")
 
         # Write the records to the output file
